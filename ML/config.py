7d60eec75d711c027cd6b3cea85ab4b93fbfc375
timcsy <timocsy@yahoo.com.tw>
2021-09-23 16:00:54 +0800

    can run a lifecycle

---
+++ ML/config.py
@@ -0,0 +1,5 @@
+# Is Debug mode or not
+DEBUG = True
+# DEBUG = False
+# Place to store the images
+image_dir = 'cameras'
--- ML/training.py
+++ ML/training.py
@@ -2,9 +2,7 @@ from mlagents_envs.base_env import ActionTuple
 from mlagents_envs.environment import UnityEnvironment
 import numpy as np
 import utils
-
-# Place to store the images
-image_dir = 'cameras'
+import config
 
 # This code is used to close an env that might not have been closed before
 try:
@@ -30,27 +28,40 @@ print('--------------------------------')
 
 # Get infomation
 print('Infomation:\n')
+
+done = False
+
+# Initial Step
+step = 0
 decision_steps, terminal_steps = env.get_steps(behavior_name)
-# Show the step information
-utils.print_observations(spec, decision_steps, image_dir, 0)
 
-for step in range(1, 50):
-  # Generate an action for all agents randomly
-  # action = spec.action_spec.random_action(len(decision_steps))
+while not done:
+  for agent_id in decision_steps:
+    # Show the step information
+    utils.print_observations(spec, decision_steps.obs, config.image_dir, step)
 
-  # Generate an action for all agents that only move forward
-  continuous_actions = np.array([[0.0]], dtype=np.float32)
-  discrete_actions = np.array([[1,0]], dtype=np.int32)
-  action = ActionTuple(continuous=continuous_actions, discrete=discrete_actions)
+    # Generate an action for all agents randomly
+    # action = spec.action_spec.random_action(len(decision_steps))
+
+    # Generate an action for all agents that only move forward
+    continuous_actions = np.array([[0.0]], dtype=np.float32)
+    discrete_actions = np.array([[1,0]], dtype=np.int32)
+    action = ActionTuple(continuous=continuous_actions, discrete=discrete_actions)
+    
+    # Set the actions
+    env.set_action_for_agent(behavior_name, agent_id, action)
+
+    # Move the simulation forward
+    env.step()
+    step += 1 # Actually, it should be increased by Decision Period, but for convenience, we just add one
+    # Get the new simulation results
+    decision_steps, terminal_steps = env.get_steps(behavior_name)
   
-  # Set the actions
-  env.set_actions(behavior_name, action)
-
-  # Move the simulation forward
-  env.step()
-
-  # Get the new simulation results
-  decision_steps, terminal_steps = env.get_steps(behavior_name)
-  # Show the step information
-  utils.print_observations(spec, decision_steps, image_dir, step)
+  for agent_id in terminal_steps:
+    done = True
+    # Show the step information
+    utils.print_observations(spec, terminal_steps.obs, config.image_dir, step)
+    print()
+    print('Done, Behavior: ' + str(behavior_name) + ', Agent: ' + str(agent_id) + ', Total Steps: ' + str(step))
+    print()
 print('--------------------------------')
